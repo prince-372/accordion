d245e9ec484c5a6fabf0302f3bf7f15293647e76
prince-372 <pmor18314@gmail.com>
2023-12-19 16:12:48 +0530

    accordian done

--- assets/js/add.js
+++
@@ -1,9 +0,0 @@
-let accordion = document.querySelectorAll(".accordion-items1")
-
-accordion.forEach(function (show) {
-    show.addEventListener("click", function () {
-        const hide = document.querySelector(".active")
-        show.classList.toggle("active")
-        hide && hide.classList.remove("active")
-    })
-});
